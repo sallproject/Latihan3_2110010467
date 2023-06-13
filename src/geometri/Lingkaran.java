5cc36c8b31af0383287450f7e138c98fd5c524ea
sallproject <mhmmd.faisal.mf.2123@gmail.com>
2023-06-13 20:57:39 +0800

    Membuat class Main dan menjalankan program

--- src/geometri/Lingkaran.java
+++ src/geometri/Lingkaran.java
@@ -1,14 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package geometri;
 
-/**
- *
- * @author ACER
- */
 public class Lingkaran {
+    private double radius;
     
+    public Lingkaran(double radius){
+        this.radius = radius;
+    }
+    
+    public double luas(){
+        return Math.PI * radius * radius;
+    }
+    
+    public double keliling(){
+        return 2 * Math.PI * radius;
+    }
 }
--- src/geometri/PersegiPanjang.java
+++ src/geometri/PersegiPanjang.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package geometri;
 
-/**
- *
- * @author ACER
- */
 public class PersegiPanjang {
+    private double panjang,lebar;
     
+    public PersegiPanjang(double panjang, double lebar){
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+    
+    public double luas(){
+        return panjang*lebar;
+    }
+    
+    public double keliling(){
+        return 2*(panjang + lebar);
+    }
 }
